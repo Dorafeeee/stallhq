4c04fe9105befc3146a630a193688ca1765a7dfd
Dorafeeee <165888093+Dorafeeee@users.noreply.github.com>
2026-04-27 22:18:30 +0100

    Fix instrumentation.js Sentry init

--- instrumentation.js
+++ instrumentation.js
@@ -2,11 +2,17 @@ import * as Sentry from "@sentry/nextjs";
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === "nodejs") {
-    await import("./sentry.server.config");
+    Sentry.init({
+      dsn: "https://17a43ecc9a3b5928b43b5823dea9fa82@o4511291596537856.ingest.de.sentry.io/4511291700936784",
+      sendDefaultPii: true,
+    });
   }
 
   if (process.env.NEXT_RUNTIME === "edge") {
-    await import("./sentry.edge.config");
+    Sentry.init({
+      dsn: "https://17a43ecc9a3b5928b43b5823dea9fa82@o4511291596537856.ingest.de.sentry.io/4511291700936784",
+      sendDefaultPii: true,
+    });
   }
 }
 
